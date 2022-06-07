ec94fbfafceaf2259f3d4d369b251484b11c94db
arona <aronadiallo90@gmail.com>
2022-06-07 18:52:07 +0000

    authentication

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from contextlib import redirect_stderr
+from django.shortcuts import render, redirect
+from . import forms
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
+
+
+
+def login_page(request):
+    form = forms.LoginForm()
+    message = ''
+    if request.method == 'POST' :
+        form = forms.LoginForm(request.POST)
+        if form.is_valid():
+            user = authenticate(username = form.cleaned_data['username'], password = form.cleaned_data['password'])
+            if user is not None:
+                login(request,user)
+                message = f'Bonjour {user.username} vous êtes connecté'
+            else:
+                message = 'Identifiant invalide'
+
+    return render(request, 'authentication/login.html', context = {'form' : form, 'message' : message})
+
+def logout_user(request):
+    logout(request)
+    return redirect('login')
+
+
