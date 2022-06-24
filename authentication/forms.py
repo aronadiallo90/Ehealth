c88c49abd9721dcfe44c88a0db994eb63cd10bca
arona <aronadiallo90@gmail.com>
2022-06-24 16:27:49 +0000

    'cc'

--- authentication/forms.py
+++ authentication/forms.py
@@ -20,9 +20,9 @@ class SignupForm(UserCreationForm,forms.Form):
 
 
 
-class LoginForm(forms.Form):
+# class LoginForm(forms.Form):
 
-    username = forms.CharField(max_length=63, label="Nom D'utilisateur")
-    password = forms.CharField(max_length=63, widget = forms.PasswordInput, label='Mot de passe')
+#     username = forms.CharField(max_length=63, label="Nom D'utilisateur")
+#     password = forms.CharField(max_length=63, widget = forms.PasswordInput, label='Mot de passe')
 
 
